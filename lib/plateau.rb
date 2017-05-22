90baec1f200f3b74abed6253e1ec5d3f1b344b5c
Sunil <Sunil@Sunils-MBPro.local>
2017-05-22 22:47:55 +0530

    Added required comments

--- lib/plateau.rb
+++ lib/plateau.rb
@@ -1,6 +1,7 @@
 class Plateau
   attr_reader :east_end, :north_end, :south_end, :west_end
 
+  # Initializing the Plateau object
   def initialize(coordinates)
     @east_end, @north_end = coordinates.split(" ").map {|x| x.to_i}
     @south_end = 0
--- lib/rover.rb
+++ lib/rover.rb
@@ -1,8 +1,7 @@
-# require '../lib/plateau';
-
 class Rover
   attr_reader :x_coordinate, :y_coordinate, :curr_direction
 
+  # Initializing the Rover object
   def initialize(plateau, position)
     x, y, @curr_direction = position.split(" ")
     @x_coordinate = x.to_i
@@ -11,11 +10,15 @@ class Rover
     @plateau = Plateau.new(plateau)
   end
 
+  # Method to execute the instructions
+  #:@params series: String
   def exec_instructions(series)
     series.split("").each {|command| action(command)}
     "#{@x_coordinate} #{@y_coordinate} #{@curr_direction}"
   end
 
+  # Perform the actions with respect to the command provided
+  #:@params command: String
   def action(command)
     case command
       when 'M' then move
@@ -24,6 +27,7 @@ class Rover
     end
   end
 
+  # Turn the rover to left
   def turning_left
     @curr_direction = case @curr_direction
                         when 'N' then 'W'
@@ -33,6 +37,7 @@ class Rover
                       end
   end
 
+  # Turn the rover to right
   def turning_right
     @curr_direction = case @curr_direction
                         when 'N' then 'E'
@@ -42,6 +47,7 @@ class Rover
                       end
   end
 
+  # Move the rover 1 step ahead wrt the current position
   def move
     x, y = case @curr_direction
              when 'N' then [0, 1] unless cannot_move?
@@ -54,6 +60,7 @@ class Rover
     @y_coordinate += y
   end
 
+  # Check if rover can move further or not
   def cannot_move?
     case @curr_direction
       when 'N' then @y_coordinate == @plateau.north_end ? true : false
